e393596d2c394f5a9533344713dffe42de738177
sitek94 <msitkowski94@gmail.com>
2021-07-06 20:17:23 +0200

    include tailwind in css

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <h1 className="text-4xl font-bold">Hello world!</h1>
     </div>
   );
 }
